3f927da70ef3bf9c95a77b94810595de9a1bf9ae
RobnettSean22 <robnettsean22@gmail.com>
2020-03-12 11:30:52 +0900

    saved selcted movie, movie price, and selected seats in local storage

--- script.js
+++ script.js
@@ -4,18 +4,36 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
+
+setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+};
 
 updateSelectedCount = () => {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const seatsIndex = [...selectedSeats].map(seat => {
+    return [...seats].indexOf(seat);
+  });
+
+  localStorage.setItem("slectedSeats", JSON.stringify(seatsIndex));
   const selectedSeatsCount = selectedSeats.length;
 
   count.innerText = selectedSeatsCount;
 
   total.innerText = selectedSeatsCount * ticketPrice;
 };
-console.log(updateSelectedCount());
+//movie select event
 
+movieSelect.addEventListener("change", e => {
+  ticketPrice = +e.target.value;
+  setMovieData = (e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+//seat select event
 container.addEventListener("click", e => {
   if (
     e.target.classList.contains("seat") &&
