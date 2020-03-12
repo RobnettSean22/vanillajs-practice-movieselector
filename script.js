a1c7346a63d72f93e9d01b16ffc98c8c94921c9d
RobnettSean22 <robnettsean22@gmail.com>
2020-03-12 20:34:32 +0900

    issue with movie select and price not saving to local storage

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
 const container = document.querySelector(".container");
-const seats = document.querySelectorAll(".row .seat:not(occupied)");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
-
+populateUI();
 let ticketPrice = +movieSelect.value;
 
-setMovieData = (movieIndex, moviePrice) => {
+function setMovieData(movieIndex, moviePrice) {
   localStorage.setItem("selectedMovieIndex", movieIndex);
   localStorage.setItem("selectedMoviePrice", moviePrice);
-};
+}
 
-updateSelectedCount = () => {
+function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
 
   const seatsIndex = [...selectedSeats].map(seat => {
     return [...seats].indexOf(seat);
   });
 
-  localStorage.setItem("slectedSeats", JSON.stringify(seatsIndex));
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
   const selectedSeatsCount = selectedSeats.length;
 
   count.innerText = selectedSeatsCount;
 
   total.innerText = selectedSeatsCount * ticketPrice;
-};
+}
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  console.log(selectedSeats);
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+}
+
 //movie select event
 
 movieSelect.addEventListener("change", e => {
   ticketPrice = +e.target.value;
-  setMovieData = (e.target.selectedIndex, e.target.value);
+
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
@@ -44,3 +58,4 @@ container.addEventListener("click", e => {
     updateSelectedCount();
   }
 });
+updateSelectedCount();
